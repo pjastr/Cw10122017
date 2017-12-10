f8d85348814a0733fce2c272df578749ca1bdd94
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-10 17:11:05 +0100

    Add files via upload

---
+++ ConsoleApplication53/ConsoleApplication53/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication53
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringLiczba = "245232";
+            string stringPusty = null;
+            string blednyFormat = "5.287";
+            string pozaZakres = "9876225432123456";
+            int liczba;
+
+            liczba = int.Parse(stringLiczba);
+            //liczba = int.Parse(stringPusty);
+            //liczba = int.Parse(blednyFormat);
+            //liczba = int.Parse(pozaZakres);
+
+            liczba = Convert.ToInt32(stringLiczba);
+            liczba = Convert.ToInt32(stringPusty);
+            //liczba = Convert.ToInt32(blednyFormat);
+            //liczba = Convert.ToInt32(pozaZakres);
+
+            bool wynik;
+            wynik = int.TryParse(stringLiczba, out liczba);
+            wynik = int.TryParse(stringPusty, out liczba);
+            wynik = int.TryParse(blednyFormat, out liczba);
+            wynik = int.TryParse(pozaZakres, out liczba);
+
+            Console.ReadKey();
+        }
+    }
+}
