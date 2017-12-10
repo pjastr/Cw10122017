257d48d7baaf1e9272152e4eedccf08d177b1b95
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-10 15:04:28 +0100

    Add files via upload

---
+++ ConsoleApplication44/ConsoleApplication44/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new NaszWyjatekException("sjdhfhsd komunikat");
+        }
+    }
+
+    class NaszWyjatekException:Exception
+    {
+        public NaszWyjatekException() { }
+
+        public NaszWyjatekException(string message) 
+            : base(message) { }
+
+        public NaszWyjatekException(String message, Exception innerException)
+            : base(message, innerException) { }
+
+
+    }
+}
---
+++ ConsoleApplication45/ConsoleApplication45/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj liczbę");
+            double a;
+            while (true)
+            {
+                try
+                {
+                    a = Convert.ToDouble(Console.ReadLine());
+                    break;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+
+            }
+            Console.WriteLine("a ={0}", a);
+            Console.ReadKey();
+        }
+    }
+}
