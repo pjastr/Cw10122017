eb927fb8e9a3e0520ae87f9b36c178563e324456
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-10 17:08:49 +0100

    Add files via upload

---
+++ ConsoleApplication49/ConsoleApplication49/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int liczba1 = 30;
+            int liczba2 = 0;
+            try
+            {
+                int liczba3 = liczba1 / liczba2;
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication50/ConsoleApplication50/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int liczba = Convert.ToInt32(Console.ReadLine());
+                Osoba os1 = new Osoba(liczba);
+            }
+
+            catch(NaszWyjatekException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("błędny format");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType());
+            }
+            
+
+            Console.ReadKey();
+        }
+    }
+
+    class Osoba
+    {
+        private int wiek;
+
+        public Osoba(int wiek)
+        {
+            if (wiek < 0) throw new NaszWyjatekException("wiek nie może być ujemny");
+            this.wiek = wiek;
+        }
+    }
+
+    class NaszWyjatekException:Exception
+    {
+        public NaszWyjatekException() { }
+
+        public NaszWyjatekException(string message) : base(message) { }
+        public NaszWyjatekException(String message, Exception innerException)
+            : base(message, innerException) { }
+
+    }
+}
