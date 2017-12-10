7a01831b2a6bf5b82e1b6024669a8f52d4a4946e
pjastr <24464854+pjastr@users.noreply.github.com>
2017-12-10 17:07:03 +0100

    Add files via upload

---
+++ ConsoleApplication47/ConsoleApplication47/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Aaa
+    {
+        public virtual void Metoda()
+        {
+            Console.WriteLine("Aaa");
+        }
+    }
+
+    class Bbb : Aaa
+    {
+        public override sealed void Metoda()
+        {
+            Console.WriteLine("bbb");
+        }
+    }
+
+    class Ccc:Bbb
+    {
+        //public override void Metoda()
+        //{
+        //    Console.WriteLine("ccc");
+        //}
+    }
+}
---
+++ ConsoleApplication48/ConsoleApplication48/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obiekt = new B();
+            obiekt.VirtualFun2();
+
+            Console.ReadKey();
+        }
+    }
+
+    public class A
+    {
+        public void NormalFun() { Console.WriteLine("A NormalFun()"); }
+        public virtual void VirtualFun() { Console.WriteLine("A VirtualFun()"); VirtualFun2(); }
+        public virtual void VirtualFun2() { Console.WriteLine("A VirtualFun2()"); }
+    }
+
+    public class B : A
+    {
+        public void NormalFun() { Console.WriteLine("B NormalFun()"); }
+        public virtual void VirtualFun() { Console.WriteLine("B VirtualFun()"); }
+        public override void VirtualFun2() { Console.WriteLine("B VirtualFun2()"); }
+    }
+
+    public class C : B
+    {
+        public override void VirtualFun() { Console.WriteLine("C VirtualFun()"); }
+    }
+
+    public class D : C
+    {
+        public void NormalFun() { Console.WriteLine("D NormalFun()"); }
+        public override void VirtualFun() { Console.WriteLine("D VirtualFun()"); }
+        public override void VirtualFun2() { Console.WriteLine("D VirtualFun2()"); }
+    }
+
+    public abstract class E : D
+    {
+        public virtual void VirtualFun() { Console.WriteLine("E VirtualFun()"); }
+        public abstract void VirtualFun2();
+    }
+
+    public class F : E
+    {
+        public override void VirtualFun() { Console.WriteLine("F VirtualFun()"); }
+        public override void VirtualFun2() { Console.WriteLine("F VirtualFun2()"); }
+    }
+}
